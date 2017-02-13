0d6a4e3b9d0cd1c991f677890836d3a290ca6e24
Claudio Salazar <csalazar@spect.cl>
2017-02-13 12:28:29 -0300

    Add new plugin: crayon_syntax_highlighter

---
+++ detectem/plugins/wordpress/crayon-syntax-highlighter.py
@@ -0,0 +1,13 @@
+from detectem.plugin import Plugin
+
+
+class CrayonSyntaxHighlighterPlugin(Plugin):
+    name = 'crayon-syntax-highlighter'
+    homepage = 'https://wordpress.org/plugins-wp/crayon-syntax-highlighter/'
+    matchers = []
+    js_matchers = [
+        {
+            'check': 'window.CrayonSyntaxSettings',
+            'version': 'window.CrayonSyntaxSettings.version',
+        },
+    ]
