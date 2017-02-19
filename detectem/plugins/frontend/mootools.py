27212915bc8cf87627814a816383e124057a4ef6
Claudio Salazar <csalazar@spect.cl>
2017-02-18 21:26:18 -0300

    Add MooTools plugins

---
+++ detectem/plugins/frontend/mootools.py
@@ -0,0 +1,22 @@
+from detectem.plugin import Plugin
+
+
+class MooToolsCorePlugin(Plugin):
+    name = 'mootools-core'
+    homepage = 'https://mootools.net/core'
+    matchers = []
+    js_matchers = [
+        {'check': 'window.MooTools', 'version': 'window.MooTools.version'},
+    ]
+
+
+class MooToolsMorePlugin(Plugin):
+    name = 'mootools-more'
+    homepage = 'https://mootools.net/more'
+    matchers = []
+    js_matchers = [
+        {
+            'check': 'window.MooTools && window.MooTools.More',
+            'version': 'window.MooTools.More.version'
+        },
+    ]
