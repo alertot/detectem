42da37ad51012e74a7b0fe12b50e7c0cda251584
Claudio Salazar <csalazar@spect.cl>
2017-03-24 19:15:17 -0300

    Bump version: 0.5.1 → 0.5.2

--- detectem/__init__.py
+++ detectem/__init__.py
@@ -1,3 +1,3 @@
 __author__ = """Claudio Salazar"""
 __email__ = 'csalazar@spect.cl'
-__version__ = '0.5.1'
+__version__ = '0.5.2'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requirements = open('tests/requirements.txt').read().splitlines()
 
 setup(
     name='detectem',
-    version='0.5.1',
+    version='0.5.2',
     description="Detect software in websites.",
     author="Claudio Salazar",
     author_email='csalazar@spect.cl',
