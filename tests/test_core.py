42a6ee73129e946caf606137f5d4a8583a53a9d2
Sebastián Mancilla <smancill@alertot.com>
2017-07-28 10:52:58 -0400

    Add unit tests for all matchers
    Add tests that check every matcher ('headers', 'body', 'url') for every
    source of matchers ('matchers', 'indicators' and 'modular_matchers').

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,7 @@
 import pytest
 
 from detectem.core import Detector, Result, ResultCollection
-from detectem.plugin import Plugin
+from detectem.plugin import Plugin, PluginCollection
 from detectem.settings import INDICATOR_TYPE, HINT_TYPE
 
 
@@ -15,6 +15,40 @@ class TestDetector():
         },
     }
 
+    URL = 'http://domain.tld/'
+
+    FOO_PLUGIN = {
+        'name': 'foo',
+        'homepage': 'foo',
+        'matchers': {
+            'url': 'foo.*-(?P<version>[0-9\.]+)\.js',
+            'header': ('FooHeader', 'Foo.* v(?P<version>[0-9\.]+)'),
+            'body': 'Foo.* v(?P<version>[0-9\.]+)',
+        },
+        'indicators': {
+            'url': 'foo.*\.js',
+            'header': ('FooHeader', 'Foo'),
+            'body': 'Foo',
+        },
+        'modular_matchers': {
+            'url': 'foo-(?P<name>\w+)-.*\.js',
+            'header': ('FooHeader', 'Foo-(?P<name>\w+)'),
+            'body': 'Foo-(?P<name>\w+)',
+        },
+    }
+
+    FOO_RESULTS = [
+        [{'name': 'foo', 'version': '1.1'}],
+        [{'name': 'foo'}],
+        [{'name': 'foo-min', 'version': '1.1'}],
+    ]
+
+    MATCHER_SOURCES = [
+        ['matchers'],
+        ['indicators'],
+        ['matchers', 'modular_matchers'],
+    ]
+
     @pytest.fixture
     def min_detector(self):
         return Detector({'har': None, 'softwares': None}, [], None)
@@ -22,6 +56,83 @@ class TestDetector():
     def test_detector_starts_with_empty_results(self):
         d = Detector({'har': None, 'softwares': None}, [], None)
         assert not d._results.get_results()
+
+    def _create_plugin(self, template, sources, matchers):
+        class TestPlugin(Plugin):
+            name = template['name']
+            homepage = template['homepage']
+
+        p = TestPlugin()
+        for s in sources:
+            g = [{m: template[s][m]} for m in matchers]
+            setattr(p, s, g)
+        return p
+
+    def _create_detector(self, har, plugins):
+        pc = PluginCollection()
+        for p in plugins:
+            pc.add(p)
+        return Detector({'har': har, 'softwares': []}, pc, self.URL)
+
+    @pytest.mark.parametrize('sources,result', zip(MATCHER_SOURCES, FOO_RESULTS))
+    def test_match_from_headers(self, sources, result):
+        har = [
+            {
+                'request': {'url': self.URL},
+                'response': {
+                    'url': self.URL,
+                    'headers': [
+                        {'name': 'FooHeader', 'value': 'Foo-min v1.1'}
+                    ]
+                },
+            },
+        ]
+        p = self._create_plugin(self.FOO_PLUGIN, sources, ['header'])
+        d = self._create_detector(har, [p])
+
+        assert d.get_results() == result
+
+    @pytest.mark.parametrize('sources,result', zip(MATCHER_SOURCES, FOO_RESULTS))
+    def test_match_from_body(self, sources, result):
+        har = [
+            {
+                'request': {'url': self.URL},
+                'response': {
+                    'url': self.URL,
+                    'content': {'text': 'Main content'},
+                },
+            },
+            {
+                'request': {'url': 'http://foo.org/lib/foo.js'},
+                'response': {
+                    'url': 'http://foo.org/lib/foo.js',
+                    'content': {'text': 'Plugin Foo-min v1.1'},
+                },
+            },
+        ]
+        p = self._create_plugin(self.FOO_PLUGIN, sources, ['body'])
+        d = self._create_detector(har, [p])
+
+        assert d.get_results() == result
+
+    @pytest.mark.parametrize('sources,result', zip(MATCHER_SOURCES, FOO_RESULTS))
+    def test_match_from_url(self, sources, result):
+        har = [
+            {
+                'request': {'url': self.URL},
+                'response': {'url': self.URL},
+            },
+            {
+                'request': {'url': 'http://foo.org/lib/foo-min-1.1.js'},
+                'response': {
+                    'url': 'http://foo.org/lib/foo-min-1.1.js',
+                },
+            },
+        ]
+        p = self._create_plugin(self.FOO_PLUGIN, sources, ['url'])
+        d = self._create_detector(har, [p])
+
+        assert d.get_results() == result
 
     @pytest.mark.parametrize('entry,result', [
         ({'request': {'url': 'http://a'}}, 'http://a'),
