46df020f5f349ac02c509e334ffd7e1f5970915b
Claudio Salazar <csalazar@spect.cl>
2017-09-06 19:41:20 -0300

    Fix in tests for exception messages

--- detectem/exceptions.py
+++ detectem/exceptions.py
@@ -9,10 +9,10 @@ class NotNamedParameterFound(Exception):
 class SplashError(Exception):
     def __init__(self, msg):
         self.msg = 'Splash error: {}'.format(msg)
-        super().__init__(msg)
+        super().__init__(self.msg)
 
 
 class NoPluginsError(Exception):
     def __init__(self, msg):
         self.msg = msg
-        super().__init__(msg)
+        super().__init__(self.msg)
