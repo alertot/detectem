a6f45dd379151abcbae10ad61aad41d04a291db0
Claudio Salazar <csalazar@spect.cl>
2017-09-06 17:56:01 -0300

    Add get_boolean_value to get settings as valid boolean values
    Add its tests too

--- detectem/settings.py
+++ detectem/settings.py
@@ -1,9 +1,25 @@
 import os
 
+
+def get_boolean_value(key, default):
+    value = None
+    string_value = os.environ.get(key)
+
+    if string_value == 'True':
+        value = True
+    elif string_value == 'False':
+        value = False
+
+    if value is None:
+        value = default
+
+    return value
+
+
 PLUGIN_PACKAGES = os.environ.get('DET_PLUGIN_PACKAGES', 'detectem.plugins').split(',')
 
 SPLASH_URL = os.environ.get('SPLASH_URL', 'http://localhost:8050')
-SETUP_SPLASH = os.environ.get('SETUP_SPLASH', 'True') == 'True'
+SETUP_SPLASH = get_boolean_value('SETUP_SPLASH', True)
 DOCKER_SPLASH_IMAGE = os.environ.get('DOCKER_SPLASH_IMAGE', 'scrapinghub/splash:latest')
 
 SPLASH_MAX_TIMEOUT = int(os.environ.get('SPLASH_MAX_TIMEOUT', '120'))
---
+++ tests/test_settings.py
@@ -0,0 +1,20 @@
+import os
+
+from unittest.mock import patch
+
+from detectem.settings import get_boolean_value
+
+
+def test_get_boolean_value_with_true():
+    with patch.dict('os.environ', {'test': 'True'}):
+        assert get_boolean_value('test', False)
+
+
+def test_get_boolean_value_with_false():
+    with patch.dict('os.environ', {'test': 'False'}):
+        assert not get_boolean_value('test', True)
+
+
+def test_get_boolean_value_with_default():
+    with patch.dict('os.environ', {'test': 'xxx'}):
+        assert get_boolean_value('test', 'default') == 'default'
