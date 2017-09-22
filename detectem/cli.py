f17a6351164bd8ed45bc15fc4618dd5c64fa8ba2
Sebastián Mancilla <smancill@alertot.com>
2017-09-22 18:00:34 -0300

    Add command line option to list plugins
    Add a '--plugins' option to the 'det' command. If set, all the
    registered plugins will be printed as a list of names. If '--metadata'
    is also set, the plugins will be printed as a list of dictionaries, with
    the name, homepage and hints (if any) of each plugin.
    
    Since click still makes the URL argument required, set it with a default
    dummy value to ensure the command doesn't fail when '--plugins' is the
    only argument given, and print the help manually when a URL is not
    passed and detection is intended.
    
    The required flag for the URL argument is to prevent the help printing
    the argument as '[URL]' (due to having a default value).

--- detectem/cli.py
+++ detectem/cli.py
@@ -2,12 +2,17 @@ import logging
 import sys
 import click
 
+from operator import attrgetter
+
 from detectem.response import get_response
 from detectem.plugin import load_plugins
 from detectem.core import Detector
 from detectem.exceptions import DockerStartError, SplashError, NoPluginsError
 from detectem.utils import create_printer
 from detectem.settings import SPLASH_TIMEOUT, CMD_OUTPUT, JSON_OUTPUT
+
+
+DUMMY_URL = "http://domain.tld"
 
 # Set up logging
 logger = logging.getLogger('detectem')
@@ -41,8 +46,18 @@ logger.addHandler(ch)
     is_flag=True,
     help='Include this flag to return plugin metadata.'
 )
-@click.argument('url')
-def main(debug, timeout, format, metadata, url):
+@click.option(
+    '--plugins', 'list_plugins',
+    default=False,
+    is_flag=True,
+    help='List registered plugins'
+)
+@click.argument('url', default=DUMMY_URL, required=True)
+def main(debug, timeout, format, metadata, list_plugins, url):
+    if not list_plugins and url == DUMMY_URL:
+        click.echo(click.get_current_context().get_help())
+        sys.exit(1)
+
     if debug:
         click.echo("[+] Enabling debug mode.")
         ch.setLevel(logging.DEBUG)
@@ -52,7 +67,10 @@ def main(debug, timeout, format, metadata, url):
 
     printer = create_printer(format)
     try:
-        results = get_detection_results(url, timeout, metadata)
+        if not list_plugins:
+            results = get_detection_results(url, timeout, metadata)
+        else:
+            results = get_plugins(metadata)
     except (NoPluginsError, DockerStartError, SplashError) as e:
         printer(str(e))
         sys.exit(1)
@@ -79,6 +97,28 @@ def get_detection_results(url, timeout, metadata):
     det = Detector(response, plugins, url)
     results = det.get_results(metadata=metadata)
 
+    return results
+
+
+def get_plugins(metadata):
+    """ Return the registered plugins.
+
+    Load and return all registered plugins.
+    """
+    plugins = load_plugins()
+    if not plugins:
+        raise NoPluginsError('No plugins found')
+
+    results = []
+    for p in sorted(plugins.get_all(), key=attrgetter('name')):
+        if metadata:
+            data = {'name': p.name, 'homepage': p.homepage}
+            hints = getattr(p, 'hints', [])
+            if hints:
+                data['hints'] = hints
+            results.append(data)
+        else:
+            results.append(p.name)
     return results
 
 
