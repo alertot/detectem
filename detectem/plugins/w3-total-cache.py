f08bcd47ea9cf3e50c70bcb3ed173b8dbd225d58
Claudio Salazar <csalazar@spect.cl>
2018-08-13 17:43:28 +0200

    Improve w3-total-cache plugin with new indicator

--- detectem/plugins/w3-total-cache.py
+++ detectem/plugins/w3-total-cache.py
@@ -3,9 +3,13 @@ from detectem.plugin import Plugin
 
 class W3TotalCachePlugin(Plugin):
     name = 'w3-total-cache'
+    vendor = 'Frederick Townes'
     homepage = 'https://wordpress.org/plugins/w3-total-cache/'
     tags = ['wordpress']
 
     matchers = [
         {'header': ('X-Powered-By', r'W3 Total Cache/(?P<version>[0-9\.]+)')},
+    ]
+    indicators = [
+        {'xpath': "//comment()[contains(.,'Performance optimized by W3 Total Cache')]"},
     ]
