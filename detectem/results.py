075ed1214558ab9a9fe2a1048346ddf5d3dd233d
Claudio Salazar <csalazar@spect.cl>
2018-08-22 17:23:04 +0200

    Move results to its own file

---
+++ detectem/results.py
@@ -0,0 +1,81 @@
+from collections import defaultdict
+from distutils.version import LooseVersion
+
+from detectem.settings import (
+    VERSION_TYPE,
+    INDICATOR_TYPE,
+    HINT_TYPE,
+    GENERIC_TYPE,
+)
+
+
+class Result():
+    def __init__(
+        self,
+        name,
+        version=None,
+        homepage=None,
+        from_url=None,
+        type=VERSION_TYPE,
+    ):
+        self.name = name
+        self.type = type
+        self.version = version
+        self.homepage = homepage
+        self.from_url = from_url
+
+    def __hash__(self):
+        return hash((self.name, self.version, self.type))
+
+    def __eq__(self, o):
+        def to_tuple(rt):
+            return (rt.name, rt.version, rt.type)
+
+        return to_tuple(self) == to_tuple(o)
+
+    def __lt__(self, o):
+        def to_tuple(rt):
+            return (rt.name, LooseVersion(rt.version or '0'), rt.type)
+
+        return to_tuple(self) < to_tuple(o)
+
+    def __repr__(self):
+        return str({'name': self.name, 'version': self.version, 'type': self.type})
+
+
+class ResultCollection():
+    def __init__(self):
+        self._results = defaultdict(list)
+
+    def add_result(self, rt):
+        self._results[rt.name].append(rt)
+
+    def _normalize_results(self):
+        norm_results = defaultdict(list)
+
+        for p_name, p_results in self._results.items():
+            rdict = defaultdict(set)
+            for rt in p_results:
+                rdict[rt.type].add(rt)
+
+            p_list = []
+            if VERSION_TYPE in rdict:
+                p_list = list(rdict[VERSION_TYPE])
+                assert len(p_list) >= 1
+            elif INDICATOR_TYPE in rdict:
+                p_list = list(rdict[INDICATOR_TYPE])
+                assert len(p_list) == 1
+            elif HINT_TYPE in rdict:
+                p_list = list(rdict[HINT_TYPE])
+                assert len(p_list) == 1
+            elif GENERIC_TYPE in rdict:
+                p_list = list(rdict[GENERIC_TYPE])
+                assert len(p_list) == 1
+
+            norm_results[p_name] = p_list
+
+        return norm_results
+
+    def get_results(self, normalize=True):
+        results = self._normalize_results() if normalize else self._results
+        return [rt for p_results in results.values() for rt in p_results]
---
+++ tests/test_results.py
@@ -0,0 +1,86 @@
+import pytest
+
+from detectem.results import Result, ResultCollection
+from detectem.settings import INDICATOR_TYPE, HINT_TYPE, GENERIC_TYPE
+
+
+class TestResultCollection():
+    @staticmethod
+    def _assert_results(detected, results):
+        c = ResultCollection()
+        for d in detected:
+            c.add_result(d)
+        assert set(c.get_results()) == set(results)
+
+    @pytest.mark.parametrize(
+        'detected,results', [
+            ([
+                Result('pluginA', '1.1'),
+                Result('pluginB', '3.8.7'),
+                Result('pluginC', '4.0')
+            ], [
+                Result('pluginA', '1.1'),
+                Result('pluginB', '3.8.7'),
+                Result('pluginC', '4.0')
+            ]),
+            (
+                [
+                    Result('pluginA', '1.3'),
+                    Result('pluginA', '1.2'),
+                    Result('pluginA', '1.1')
+                ],
+                [
+                    Result('pluginA', '1.1'),
+                    Result('pluginA', '1.2'),
+                    Result('pluginA', '1.3')
+                ],
+            ),
+            ([
+                Result('pluginA', '1.1'),
+                Result('pluginC', type=HINT_TYPE),
+                Result('pluginB', type=INDICATOR_TYPE),
+                Result('pluginD', type=GENERIC_TYPE),
+            ], [
+                Result('pluginA', '1.1'),
+                Result('pluginB', type=INDICATOR_TYPE),
+                Result('pluginC', type=HINT_TYPE),
+                Result('pluginD', type=GENERIC_TYPE),
+            ]),
+        ]
+    )
+    def test_get_all_detected_plugins(self, detected, results):
+        self._assert_results(detected, results)
+
+    @pytest.mark.parametrize(
+        'detected,results', [
+            ([
+                Result('pluginA', '1.1'),
+                Result('pluginA', '1.2'),
+                Result('pluginA', '1.1'),
+            ], [Result('pluginA', '1.1'),
+                Result('pluginA', '1.2')]),
+            ([
+                Result('pluginA', '1.1'),
+                Result('pluginA', type=INDICATOR_TYPE),
+                Result('pluginA', type=HINT_TYPE),
+            ], [Result('pluginA', '1.1')]),
+            ([
+                Result('pluginB', type=HINT_TYPE),
+                Result('pluginB', type=HINT_TYPE),
+            ], [Result('pluginB', type=HINT_TYPE)]),
+            ([
+                Result('pluginB', type=INDICATOR_TYPE),
+                Result('pluginB', type=INDICATOR_TYPE)
+            ], [Result('pluginB', type=INDICATOR_TYPE)]),
+            ([
+                Result('pluginB', type=INDICATOR_TYPE),
+                Result('pluginB', type=HINT_TYPE),
+            ], [Result('pluginB', type=INDICATOR_TYPE)]),
+            ([
+                Result('pluginB', type=INDICATOR_TYPE),
+                Result('pluginB', type=GENERIC_TYPE),
+            ], [Result('pluginB', type=INDICATOR_TYPE)]),
+        ]
+    )
+    def test_remove_duplicated_results(self, detected, results):
+        self._assert_results(detected, results)
