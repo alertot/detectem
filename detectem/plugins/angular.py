5a7a9d59fcee41c4080c51263e8a32b20a1c0244
Alexei Volynshchikov <alex.volynshchikov@techusage.com>
2019-10-21 19:58:53 +0700

    change angular dom matcher to not callable str

--- detectem/plugins/angular.py
+++ detectem/plugins/angular.py
@@ -8,7 +8,7 @@ class AngularPlugin(Plugin):
     matchers = [
         {'body': r'<[^>]+ ng-version=\"(?P<version>[0-9a-z\.-]+)\"'},
         {'dom': (
-            'window.getAllAngularRootElements()',
+            'window.getAllAngularRootElements',
             'window.getAllAngularRootElements()[0].attributes["ng-version"].value'
         )},
         {'dom': ('window.ng && window.ng.coreTokens', None)},
