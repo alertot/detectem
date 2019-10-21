2c0a93963bedc325ac3604e7ced2054a31719f6e
Alexei Volynshchikov <alex.volynshchikov@techusage.com>
2019-10-21 20:03:28 +0700

    add emberjs plugin

---
+++ detectem/plugins/emberjs.py
@@ -0,0 +1,11 @@
+from detectem.plugin import Plugin
+
+
+class EmberJSPlugin(Plugin):
+    name = 'ember'
+    homepage = 'http://emberjs.com'
+    tags = ['ember', 'javascript', 'js framework']
+
+    matchers = [
+        {'dom': ('window.Ember', 'window.Ember.VERSION')},
+    ]
